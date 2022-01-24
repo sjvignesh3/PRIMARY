bb1819b39d3b920ba0391a167c70626bb23cbbef
Vigneshwaran J <sjvignesh333@gmail.com>
2022-01-24 20:10:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
     include 'snippets/header.php';
     echo '<br/>';
-    echo '<a href="https://www.heroku.com" target="_blank" > Heroku.com </a>';
+    echo '<a href="https://www.heroku.com" target="_blank" > Heroku.com </a><br/>';
    // require 'snippets/footer.php';
 ?>
 
-
+<?php
+    include 'snippets/footer.php'
+?>
 </body>
 </html>
